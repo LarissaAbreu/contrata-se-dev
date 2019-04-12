4f8ae4fa0caeb44d02bf6b3ae4fa2dd2f895ddf1
Larissa Abreu <larissa.a.abreu@hotmail.com>
2019-04-11 22:28:22 -0300

    Update style

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -7,12 +7,13 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      issues: []
+      issues: [],
+      pages: 1
     };
   };
 
   componentDidMount() {
-    fetch('https://api.github.com/repos/frontendbr/vagas/issues?state=open')
+    fetch('https://api.github.com/repos/frontendbr/vagas/issues?state=open&page=1')
       .then((res) => res.json())
       .then((json) => this.setState({issues: json}));
   };
@@ -23,6 +24,15 @@ class App extends React.Component {
 
     return `${ymd[2]}/${ymd[1]}/${ymd[0]}`;
   };
+
+  _showMore() {
+    fetch(`https://api.github.com/repos/frontendbr/vagas/issues?state=open&page=${this.state.pages + 1}`)
+      .then((res) => res.json())
+      .then((json) => {
+        const issues = [...this.state.issues, ...json];
+        this.setState({issues: issues, pages: this.state.pages + 1})
+      })
+  }
 
   render () {
     const $issues = this.state.issues.map((issue, i) => {
@@ -50,12 +60,16 @@ class App extends React.Component {
     return (
       <div>
         <div className="topo">
-          <h1 className="topo__titulo">Contrata-se dev</h1>
-          <p className="topo__texto">Veja abaixo uma relação de empresas que estão precisando de devs... Boa sorte :D</p>
-          <a href="https://github.com/frontendbr/vagas/issues/new" target="_blank" className="nova-vaga">Postar uma nova vaga</a>
+          <h1 className="topo__titulo">Contrata-se.dev</h1>
+          <p className="topo__texto">Agregador de vagas para pessoas desenvolvedoras :)</p>
+          <div className="social">
+            <a href="https://github.com/frontendbr/vagas/issues/new" target="_blank" className="nova-vaga">Postar uma nova vaga</a>
+            <iframe src="https://ghbtns.com/github-btn.html?user=LarissaAbreu&repo=contrata-se-dev&type=star&size=large" frameborder="0" scrolling="0" width="75.43px" height="30px"></iframe>
+          </div>
         </div>
         <div className="main">
           <div className="lista-vagas">{$issues}</div>
+          <button className="mais" onClick={() => this._showMore()}>Mostrar mais</button>
         </div>
         <div className="rodape">
           <span>Feito com</span><span className="rodape__heart"></span>
